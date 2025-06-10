c59935cb18f927df9c43d7730e805c6ecab9f170
tanishq <tanishq@gmai.com>
2025-06-10 19:20:49 +0530

    first commit

---
+++ MainWindow.cpp
@@ -0,0 +1,12 @@
+#include "MainWindow.hpp"
+
+MainWindow::MainWindow(QWidget *parent) 
+    : QMainWindow(parent)
+{
+    setWindowTitle("To-Do Management");
+    resize(800,600);
+}
+
+MainWindow::~MainWindow() {
+
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# To-Do_Qt_App
